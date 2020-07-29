98aa027df18e698d3be660ca2f880323cd78e4ed
Asquidy <afradkin@gmail.com>
2020-07-29 11:38:38 -0400

    try again with compute_se

--- pyblp/economies/problem.py
+++ pyblp/economies/problem.py
@@ -626,11 +626,12 @@ class ProblemEconomy(Economy):
                 output("Updating the weighting matrix ...")
             else:
                 output("Estimating standard errors ...")
-                if not compute_se:
-                    compute_gradient = False
+
+            # if not compute_se:
+            # compute_gradient = False
 
             final_progress = compute_step_progress(
-                theta, progress, compute_gradient, compute_hessian, compute_micro_covariances
+                theta, progress, compute_se, compute_hessian, compute_micro_covariances
             )
             optimization_stats.evaluations += 1
             results = ProblemResults(
